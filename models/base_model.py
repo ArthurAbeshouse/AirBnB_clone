4fbf0dbf30415e0724197636c48a09fdfca5cb39
Anthony <huggins9000211@gmail.com>
2020-02-14 18:47:18 -0500

    test

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import models
 import uuid
 from datetime import datetime
 
+
 class BaseModel():
     """ Base """
 
@@ -17,7 +18,7 @@ class BaseModel():
                     setattr(self, x, y)
             except TypeError:
                 pass
-        else:   
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -25,7 +26,8 @@ class BaseModel():
 
     def __str__(self):
         """ Str """
-        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """ Save """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ File Storage """
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
 import json
 import models
 import copy
@@ -36,7 +42,20 @@ class FileStorage():
                 loadedString = f.read()
                 newDict2 = json.loads(loadedString)
                 for x, y in newDict2.items():
-                    newDict2[x] = BaseModel(y)
+                    if x.split(".")[0] == "BaseModel":
+                        newDict2[x] = BaseModel(y)
+                    elif x.split(".")[0] == "User":
+                        newDict2[x] = User(y)
+                    elif x.split(".")[0] == "State":
+                        newDict2[x] = State(y)
+                    elif x.split(".")[0] == "City":
+                        newDict2[x] = City(y)
+                    elif x.split(".")[0] == "Amenity":
+                        newDict2[x] = Amenity(y)
+                    elif x.split(".")[0] == "Place":
+                        newDict2[x] = Place(y)
+                    elif x.split(".")[0] == "Review":
+                        newDict2[x] = Review(y)
                 self.__objects = newDict2
         except FileNotFoundError:
             pass
