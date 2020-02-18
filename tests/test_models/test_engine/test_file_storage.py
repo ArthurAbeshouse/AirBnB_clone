1bc24a3493532e2f44a9f3ff42dd4da2780fe2b1
ArthurAbeshouse <artieabe99@gmail.com>
2020-02-18 23:51:20 +0000

    added method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
+""" FileStoage unit tests """
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """tests FileStorage"""
-    pass
+    """ Tests FileStorage """
+    def test_FileStorage(self):
+        """ Pass """
+        pass
 
 if __name__ == '__main__':
     unittest.main()
