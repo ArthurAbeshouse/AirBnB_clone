c7390b933008d4943cec19c1234797a29d4c4090
ArthurAbeshouse <artieabe99@gmail.com>
2020-02-19 02:12:37 +0000

    more fixes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,13 +3,22 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+import os
 
 
 class TestFileStorage(unittest.TestCase):
     """ Tests FileStorage """
+
     def test_FileStorage(self):
-        """ Pass """
+        """pass"""
         pass
+
+    def tearDown(self):
+        """Tears down testing methods"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
 if __name__ == '__main__':
     unittest.main()
