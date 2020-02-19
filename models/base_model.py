90d1bdec74439ddfece392f87200e89d4a67d78c
ArthurAbeshouse <artieabe99@gmail.com>
2020-02-19 00:30:17 +0000

    from models import storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Base Model """
-import models
+from models import storage
 import copy
 import uuid
 from datetime import datetime
@@ -23,7 +23,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """ Str """
@@ -33,7 +33,7 @@ class BaseModel():
     def save(self):
         """ Save """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """ to Dict """
